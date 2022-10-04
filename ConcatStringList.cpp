dc130644fe965c1946b6288e90a7c99ae51d92ca
luanvohcmut2911 <luan.vo2003@hcmut.edu.vn>
2022-10-04 12:20:57 +0700

    add ConcatStringList.cpp and .h

---
+++ ConcatStringList.cpp
@@ -0,0 +1,344 @@
+#include "ConcatStringList.h"
+
+ConcatStringList::ReferencesList ConcatStringList::refList = ConcatStringList::ReferencesList();
+ConcatStringList::DeleteStringList ConcatStringList::delStrList = ConcatStringList::DeleteStringList();
+
+string reverseStr(string s){
+    string result="";
+    for(int i=s.length()-1;i>=0;i--){
+        result+=s[i];
+    }
+    return result;
+}
+//Implementation of CharALNode
+CharALNode::CharALNode(const char* s){
+    this->CharArrayList = string(s);
+    this->next = NULL;
+    this->size = string(s).length();
+}
+CharALNode* CharALNode::splitHead(int k){
+    CharALNode *newNode = new CharALNode(this->CharArrayList.substr(k).c_str());
+    return newNode;
+}
+CharALNode* CharALNode::splitTail(int k){
+    CharALNode *newNode = new CharALNode(this->CharArrayList.substr(0,this->CharArrayList.length()-k).c_str());
+    return newNode;
+}
+//Implementation of CSList
+ConcatStringList::ConcatStringList(const char *s){
+    CharALNode *newNode = new CharALNode(s);
+    this->head = newNode;
+    this->tail = head;
+    this->size = string(s).length();
+    refList.Add(head);
+    refList.Add(tail);
+}
+ConcatStringList::ConcatStringList(CharALNode *head, CharALNode *tail){
+    this->head = head;
+    this->tail = tail;
+    this->size = 0;
+    CharALNode *temp = head;
+    while(temp!=NULL){
+        this->size += temp->size;
+        temp = temp->next;
+    }
+    refList.Add(head);
+    refList.Add(tail);
+}
+int ConcatStringList::length() const{
+    return this->size;
+}
+char ConcatStringList::get(int index) const{
+    if(index < this->size && index>= 0){
+        char result;
+        int traverse = 0;
+        CharALNode *temp = head;
+        while(temp != NULL){
+            if(index >= traverse + temp->size){
+                traverse += temp->size;
+                temp = temp->next;
+            }
+            else {
+                result = temp->CharArrayList[index-traverse];
+                break;
+            }
+        }
+        return result;
+    }
+    else{
+        throw out_of_range("Index of string is invalid!");
+    }
+}
+int ConcatStringList::indexOf(char c) const{
+    int index = -1;
+    string str = this->toString();
+    for(int i=0;i<str.length();i++){
+        if(str[i]==c) {
+            index=i;
+            break;
+        }
+    }
+    return index;
+}
+std::string ConcatStringList::toString() const{
+    string newStr = "";
+    CharALNode *temp = this->head;
+    while(temp!=NULL){
+        // cout<<temp->getArrayList()<<" "<<endl;
+        // temp = temp->getNext();
+        newStr+=temp->CharArrayList;
+        temp = temp->next;
+    }
+    return newStr;
+}
+ConcatStringList ConcatStringList::concat(const ConcatStringList & otherS)const{
+    this->tail->next = otherS.head;
+    ConcatStringList newNode = ConcatStringList(this->head,otherS.tail);
+    return newNode;
+}
+ConcatStringList ConcatStringList::subString (int from, int to)const{
+    if(from>=to){
+        throw logic_error("Invalid range");
+    }
+    else if(from<0||to<0||from>this->length()||to>this->length()){
+        throw out_of_range("Index of string is invalid");
+    }
+    else{// still not handle in the same node and do not split tail
+        CharALNode *head, *tail;
+        int findFrom = 0, findTo = 0;
+        CharALNode *tempHead = this->head, *tempTail = this->head;
+        //find head
+        while(tempHead != NULL){
+            if(findFrom + tempHead->size <= from){
+                findFrom += tempHead->size;
+            }
+            else{
+                head = tempHead->splitHead(from-findFrom);
+                break;
+            }
+            tempHead = tempHead->next;
+        }
+        //find tail
+        while(tempTail != NULL){
+            if(findTo + tempTail->size <= to){
+                findTo += tempTail->size;
+            }
+            else{
+                findTo += tempTail->size;
+                tail = tempTail->splitTail(findTo-to);
+                break;
+            }
+            tempTail = tempTail->next;
+        }
+        if(tempHead!=tempTail){
+            CharALNode *temp = tempHead->next;
+            CharALNode *runPtr = head;
+            while(temp!=tempTail){
+                CharALNode *newNode = new CharALNode(temp->CharArrayList.c_str());
+                runPtr->next = newNode;
+                runPtr = runPtr->next;
+                temp = temp->next;
+            }
+            if(tail->CharArrayList!=""){
+                runPtr->next = tail;
+            }
+            else{ //14==14
+                runPtr = tail;
+                runPtr->next = NULL;
+            }
+        }
+        else{
+            head = new CharALNode(tempHead->CharArrayList.substr(from-findFrom,to-from).c_str());
+            tail = head;
+        }
+        ConcatStringList subStr = ConcatStringList(head,tail);
+        refList.Add(subStr.head);
+        refList.Add(subStr.tail);
+        return subStr;
+    }
+}
+ConcatStringList ConcatStringList::reverse ()const{
+    CharALNode *headCSList = new CharALNode(reverseStr(this->tail->CharArrayList).c_str());
+    CharALNode *pivot = this->tail;
+    CharALNode *runPtr = headCSList;
+    if(pivot==head){
+        ConcatStringList reverseCSList = ConcatStringList(headCSList,headCSList);
+        refList.Add(reverseCSList.head);
+        refList.Add(reverseCSList.tail);
+        return reverseCSList;
+    }
+    else{
+        while(pivot!=head->next){
+            CharALNode *temp = this->head;
+            while(temp->next != pivot) temp = temp->next;
+            runPtr->next = new CharALNode(reverseStr(temp->CharArrayList).c_str());//add next
+            //go to next;
+            pivot = temp;
+            runPtr = runPtr->next;
+        }
+        CharALNode *tailCSList = new CharALNode(reverseStr(this->head->CharArrayList).c_str());
+        runPtr->next = tailCSList;
+        ConcatStringList reverseCSList = ConcatStringList(headCSList,tailCSList);
+        refList.Add(reverseCSList.head);
+        refList.Add(reverseCSList.tail);
+        return reverseCSList;
+    }
+}
+ConcatStringList::~ConcatStringList(){
+    refList.Remove(this->head);
+    refList.Remove(this->tail);
+}
+//Implement ConcatStringList::ReferenceList
+ConcatStringList::ReferencesList::ReferencesList(){
+    this->head = new RefNode();
+    this->sizeRL = 0;
+}
+void ConcatStringList::ReferencesList::Add(CharALNode *node){
+    RefNode *temp = head;
+    RefNode *newNode = new RefNode(node,1,NULL);//create new node;
+    bool added = false;
+    //added in the same node
+    while(temp!=NULL){
+        if(temp->node==node) {
+            temp->data++;
+            added = true;
+            break;
+        }
+        temp = temp->next;
+    } 
+    //added in another node --> always add in the end
+    temp = head;// redeclare temp
+    if(!added){
+        while(temp->next!=NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        sizeRL++;
+    }  
+
+}
+void ConcatStringList::ReferencesList::Insert(int data, CharALNode *node){
+    RefNode *newNode = new RefNode(node,data,NULL);
+    if(data == 0){
+        RefNode *temp = head;
+        while(temp->next!=NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+    else{
+        cout<<"current refList: "<<endl;
+        this->refCountsString();
+        cout<<"end"<<endl;
+        // RefNode* prevPtr = head->next;
+        // if(!prevPtr){
+        //     cout<<"NULL"<<endl;
+        //     head->next = newNode;
+        // }
+        // else if(!prevPtr->next){
+        //     prevPtr->next = newNode;
+        // }
+        // else{
+            
+        //     while(prevPtr->next != NULL){
+        //         if(prevPtr->next->data >= data){
+        //             newNode->next = prevPtr->next;
+        //             prevPtr->next = newNode;
+        //             break;
+        //         }
+        //         else{
+        //             prevPtr = prevPtr -> next;
+        //         }
+        //     }
+        // }
+        
+    }
+}
+int ConcatStringList::ReferencesList::refCountAt(int index)const {
+    if(index<0||index>=sizeRL) throw out_of_range("Index of string is invalid!");
+
+    else{
+        RefNode *temp = head->next;
+        for(int i=1;i<=index;i++){
+            temp = temp->next;
+        }
+        return temp->data;
+    }
+}
+std::string ConcatStringList::ReferencesList::refCountsString()const {
+    string result = "RefCounts[";
+    RefNode *temp = this->head->next;
+    if(temp!=NULL){
+        while(temp->next!=NULL){
+            result+=to_string(temp->data);
+            result+=',';
+            temp = temp->next;
+        }
+        result+=to_string(temp->data);
+    }
+    result+=']';
+    return result;
+}
+int ConcatStringList::ReferencesList::size()const {
+    return this->sizeRL;
+}
+void ConcatStringList::ReferencesList::removeNode(RefNode *node){
+    RefNode *prevPtr = head;
+    while(prevPtr->next != node&&prevPtr->next!=NULL){
+        prevPtr = prevPtr->next;
+    }
+    RefNode *nextNode = prevPtr->next->next;
+    delete prevPtr->next;
+    prevPtr->next = nextNode;
+}
+void ConcatStringList::ReferencesList::Remove(CharALNode *node){
+    RefNode *temp = head;
+    while(temp!=NULL){
+        if(temp->node == node){
+            temp->data--;
+            //sort in increment, 0 in the end
+            int count = temp->data;
+            CharALNode *saveNode = temp->node;
+            if(count == 0){
+                this->removeNode(temp);
+                this->Insert(count, saveNode);
+            }
+            else{
+                this->Insert(count, saveNode);
+                // this->removeNode(temp);
+            }
+            //clear if head = 0
+            //head->next : head
+            if(this->head->next->data==0){
+                head->next = NULL;
+                sizeRL = 0;
+            }
+            break;
+        }
+        temp = temp->next;
+    }
+}
+ConcatStringList::ReferencesList::~ReferencesList(){
+    RefNode *temp = head;
+    while(head!=NULL){
+        head = head->next;
+        delete temp;
+        temp = head;
+    }
+    sizeRL = 0;
+}
+//Implement ConcatStringList::DeleteStringList
+ConcatStringList::DeleteStringList::DeleteStringList(){
+    this->head = NULL;
+    this->sizeDL = 0;
+}
+int ConcatStringList::DeleteStringList::size()const {
+    return this->sizeDL;
+}
+std::string ConcatStringList::DeleteStringList::totalRefCountsString()const {
+    string result = "TotalRefCounts[]";
+    return result;
+}
+ConcatStringList::DeleteStringList::~DeleteStringList(){
+
+}
---
+++ ConcatStringList.h
@@ -0,0 +1,95 @@
+#ifndef __CONCAT_STRING_LIST_H__
+#define __CONCAT_STRING_LIST_H__
+
+#include "main.h"
+string reverseStr(string s);
+
+class CharALNode {
+private:
+    string CharArrayList;
+    CharALNode *next;
+    int size;
+    friend class ConcatStringList;// CSList can access to private attribute of CharALNode
+public:
+    CharALNode(const char* s = "");// constructor
+    CharALNode* splitHead(int k);// for substr
+    CharALNode* splitTail(int k);// for substr
+    ~CharALNode();// destructor
+};
+
+
+class ConcatStringList {
+public:
+    class ReferencesList; // forward declaration
+    class DeleteStringList; // forward declaration
+
+public:
+    static ReferencesList refList;
+    static DeleteStringList delStrList;
+
+    // TODO: may provide some attributes
+public:
+    CharALNode *head;
+    CharALNode *tail;
+    int size;//length of charArrayList
+public:
+    ConcatStringList(const char *s = "");// constructor O(n)
+    ConcatStringList(CharALNode *head, CharALNode *tail);// constructor for concat O(n)
+    int length() const;//get length of CSList O(1)
+    char get(int index) const;// get index O(k) k: num of CharALNode
+    int indexOf(char c) const;// O(l) l: size of CSList
+    std::string toString() const;// O(l) l: size of CSList
+    ConcatStringList concat(const ConcatStringList & otherS) const;// O(1)
+    ConcatStringList subString(int from, int to) const;
+    ConcatStringList reverse() const;
+    ~ConcatStringList();
+
+public:
+    class RefNode{// use for referenceList
+    public: 
+        int data = 0;
+        CharALNode *node;
+        RefNode *next;
+    public:
+        RefNode(CharALNode *node = new CharALNode(), int data = 0, RefNode *next = NULL){
+            this->node = node;
+            this->data = data;
+            this->next = next;
+        }
+        ~RefNode(){}
+    };
+    class ReferencesList {
+        // TODO: may provide some attributes
+        private:
+            RefNode *head;
+            int sizeRL;
+        public:
+            ReferencesList();
+            void Add(CharALNode *node);// add a node to refList
+            void Insert(int data, CharALNode *node);// insertion sort refNode
+            void Remove(CharALNode *node);
+            int size() const;
+            int refCountAt(int index) const;
+            void removeNode(RefNode *node);
+            std::string refCountsString() const;
+            ~ReferencesList();
+    };
+    class DeletedNode{
+    public: 
+
+    };
+    class DeleteStringList {
+        // TODO: may provide some attributes
+        private:
+            CharALNode *head;
+            CharALNode *tail;
+            int sizeDL;
+        public:
+            DeleteStringList();
+            int size() const;
+            std::string totalRefCountsString() const;
+            ~DeleteStringList();
+    };
+};
+
+#endif // __CONCAT_STRING_LIST_H__
